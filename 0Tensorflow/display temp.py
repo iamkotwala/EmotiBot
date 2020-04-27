e48874c7f206cb5253951eb499ac76771c2974a0
Vraj Kotwala <iamkotwala@gmail.com>
2020-04-27 16:52:44 +0530

    snap from cv using tk

--- 0Tensorflow/display temp.py
+++ 0Tensorflow/display temp.py
@@ -6,24 +6,11 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#import os
-#----------------------------------------------------
-import tkinter as tk
+from tkinter import *
 from PIL import Image, ImageTk
+import cv2
 
-#----------------------------------------------------Set up GUI
-window = tk.Tk()  #Makes main window
-window.wm_title("Digital Microscope")
-window.config(background="#FFFFFF")
-
-#Graphics window
-imageFrame = tk.Frame(window, width=600, height=500)
-imageFrame.grid(row=0, column=0, padx=10, pady=2)
-
-#Capture video frames
-lmain = tk.Label(imageFrame)
-lmain.grid(row=0, column=0)
-
+#import os
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # command line argument
@@ -31,6 +18,41 @@ ap = argparse.ArgumentParser()
 ap.add_argument("--mode", help="train/display")
 a = ap.parse_args()
 mode = a.mode
+
+win = Tk()
+win.title('EMOTIBOT')
+win.config(background = "#D9D9D9")
+win.resizable(width=FALSE, height=FALSE)
+
+#frame1 for webcamera
+frame1 = Frame(win, width=600, bg="black",height=300, padx=10, pady=10,highlightbackground="grey", highlightcolor="black", highlightthickness=5)
+frame1.pack(side=LEFT ,fill=Y,padx=10, pady=10)
+
+
+
+#frame2 for logo
+frame2 = Frame(win, bg='#D9D9D9', width=300, height=5,padx=10, pady=10,highlightbackground="grey", highlightcolor="black")
+frame2.pack(side = TOP,fill=X,padx=10) 
+photo = PhotoImage(file='C:\\Users\\iamvr\\Desktop\\EmotiBot\\LogosDesign\\logom.png')
+l = Label(frame2, image=photo,padx=10,pady=10)
+l.pack()
+l1 = Label(frame2, text="logs",font=("Helvetica", 16),bg="#D9D9D9", fg="#0E7A3F")
+l1.pack(side=BOTTOM,fill=Y,padx=2)
+
+#frame3 for logs
+frame3 = Frame(win, bg='black', width=300, height=150,padx=5, pady=5,highlightbackground="#707173", highlightcolor="black")
+frame3.pack(fill=X,expand=True,padx=5, pady=5)
+frame3.pack_propagate(0) #stops frame from shrinking
+
+ 
+#frame4 for buttons
+frame4 = Frame(win, bg='grey', width=300, height=60,padx=5, pady=5,highlightbackground="grey", highlightcolor="black")
+frame4.pack(side=BOTTOM, fill=BOTH,expand=1,padx=5,pady=10)
+
+b = Button(frame4, text='ON', padx = 40)
+b.pack(side=LEFT,pady = 20, padx = 20)
+c = Button(frame4, text='OFF', padx=40)
+c.pack(side=RIGHT,pady = 20, padx = 20)
 
 # Define data
 train_dir = 'data/train'
@@ -83,37 +105,6 @@ model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5)) #dropout was udes to minimize overfitting.
 model.add(Dense(7, activation='softmax')) #output layer
 
-
-def show_frame():
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_frame)
-
-    # Find haar cascade to draw bounding box around face
-    # ret, frame = cap.read()
-    # if not ret:
-    #   break
-    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img)
-        maxindex = int(np.argmax(prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (255, 255, 255), 2, cv2.LINE_AA)
-
-    cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
-
-
 # emotions will be displayed on your face from the webcam feed
 if mode == "display":
     model.load_weights('model.h5')
@@ -124,21 +115,43 @@ if mode == "display":
     # dictionary which assigns each label an emotion (alphabetical order)
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-
-
     # start the webcam feed
     cap = cv2.VideoCapture(0)
-    while True:
-        # Slider window (slider controls stage position)
-        sliderFrame = tk.Frame(window, width=600, height=100)
-        sliderFrame.grid(row=600, column=0, padx=10, pady=2)
+    
+    def show_frame():
+        _, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        #cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        
+        #while True:
+        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(cv2image,scaleFactor=1.3, minNeighbors=5)
 
-        show_frame()  # Display 2
-        window.mainloop()  # Starts GUI
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+        
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        lmain.imgtk = imgtk
+        lmain.configure(image=imgtk)
+        lmain.after(10, show_frame) 
+        
+    #Capture video frames
+    lmain = Label(frame1)
+    lmain.grid(row=0, column=0)
+    
+    show_frame()  #Display 2
+    win.mainloop()
+     
 
-
-    cap.release()
-    cv2.destroyAllWindows()
+    #cap.release()
+    #cv2.destroyAllWindows()
